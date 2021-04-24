74fe7957af71e2dd6dff3e60c22fcc484aeedeee
isaac025 <isaac.lopez@upr.edu>
2021-04-24 13:07:25 -0400

    init

--- scripts/step2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install xorg git build-essential libpam0g-dev libxcb-xkb-dev kitty openssh-server curl wget zsh
--- scripts/step3.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
