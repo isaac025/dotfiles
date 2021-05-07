5523d74fac1ae30fd9abc9bbbbaaae5cd2e0cdb2
isaac025 <isaac025@users.noreply.github.com>
2021-05-07 14:12:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install
+sudo apt-get install libcairo2-dev libpango1.0-dev libglib2.0-dev libimlib2-dev libgtk2.0-dev libxinerama-dev libx11-dev libxdamage-dev libxcomposite-dev libxrender-dev libxrandr-dev librsvg2-dev libstartup-notification0-dev
